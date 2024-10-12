0fa68b946edb224945f01b7a53400d6608048e9c
Jessen Forbush <for21011@byui.edu>
2024-10-12 10:55:43 -0600

    Fixed Discover Page

--- chamber/scripts/menuToggle.js
+++ chamber/scripts/menuToggle.js
@@ -2,5 +2,5 @@ const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
 hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("show"); /* Toggle class to show or hide the menu */
+  navMenu.classList.toggle("active"); // Toggle class to show or hide the menu
 });
